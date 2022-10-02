2af18f3a5aa170037aebe5211d888c98eada9c07
migueldelima <migueldelimamail@gmail.com>
2022-10-02 00:51:18 -0300

    Entrega 3.3
    (let's hope nothing is broken here)

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 const CurrPrID /*current product URL*/ = PRODUCT_INFO_URL + localStorage.getItem("prodID") + EXT_TYPE 
+const ProdComm = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("prodID") + EXT_TYPE
 
 function showProductInfo(){
     document.getElementById("prname").innerHTML = `<h2>${info.name}</h2>`
@@ -6,14 +7,44 @@ function showProductInfo(){
     document.getElementById("priceP").innerHTML = `UYU ${info.cost}`
     document.getElementById("catP").innerHTML = info.category
     document.getElementById("countP").innerHTML = `${info.soldCount}`
-    
-    
     for (let i = 0; i < info.images.length; i++) {
         document.getElementById("imgP").innerHTML += `<img style="max-width: 200px" src="${info.images[i]}">`
     }
+
 }
 
+function showComments(){
+    
+    let htmlContentToAppend = "";
+    for(let i = 0; i < comms.length; i++){
+        comment = comms[i]
+        let stars = ""
+        console.log(stars)
+        for (let h = 0; h < 5; h++) {
+            if(h<comment.score){
+                stars += `<span class="fa fa-star checked"></span>`
+            }else{
+                stars += `<span class="fa fa-star"></span>`
+            }
+        }
+        console.log(stars)
 
+    htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+              <div class="row">
+                  <div class="col">
+                      <div class="d-flex w-100 justify-content-between">
+                          <h4 class="mb-1">${comment.user} - ${stars}</h4>
+                          <small class="text-muted" id="ratingP">${comment.dateTime}</small>
+                      </div>
+                      <p class="mb-1">${comment.description}</p>
+                  </div>
+              </div>
+            </div>
+            `
+        }
+        document.getElementById("comment-container").innerHTML = htmlContentToAppend;
+}
 
 
 
@@ -26,6 +57,13 @@ document.addEventListener("DOMContentLoaded", function(e){
             info = resultObj.data
             showProductInfo()
             console.log(info)
+        }
+    })
+    getJSONData(ProdComm).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            comms = resultObj.data
+            showComments()
+            console.log(comms)
         }
     })
 })
