7071f35281b9ee080056d5db661d9042389e9d55
hylenne <migueldelimamail@gmail.com>
2022-10-26 14:34:23 -0300

    E6.1.1
    =/==/=== fuck me

--- js/cart.js
+++ js/cart.js
@@ -4,8 +4,8 @@ let cartItems = []
 let currentItem = {}
 let amount = null
 let subtotal = 0
+let subtotalAll = 0
 let price = 0
-
 
 const table = document.getElementById("cart")
 const currentCartUrl = CART_INFO_URL + userID + ".json"
@@ -15,16 +15,30 @@ function addProduct(pr) {
     <td><img src="${pr.image}" class="smallimg"></td>
     <td>${pr.name} (${pr.id})</td>
     <td>${pr.unitCost} ${pr.currency}</td>
-    <td><input class="amount" type="number" oninput="subtotalCalc(${pr.id})" style="max-width: 100px;"></td>
-    <td class="subtotal"></td></tr>`)}
+    <td><input id="amount${pr.id}" class="amount" type="number" oninput="subtotalCalc(${pr.id})" style="max-width: 100px;" value=0></td>
+    <td id="subtotal${pr.id}"></td></tr>`)
+    console.log(pr)
+    console.log(pr.currency)}
 
 function subtotalCalc(prID){
-    
-    amount = document.getElementById(prID).children[3].children[0].value
+    amount = document.getElementById(`amount${prID}`).value
     if ((amount != undefined) && (amount != "") && (parseInt(amount)) >= 0){amount = parseInt(amount)}
     price = currentItem.unitCost
     subtotal = amount * price
-    document.getElementById(prID).children[4].innerHTML = `${subtotal} ${currentItem.currency}`
+    document.getElementById(`subtotal${prID}`).innerHTML = `${subtotal} ${currentItem.currency}`
+
+    cartItems[cartItems.findIndex((found) => found = prID)].subtotal = subtotal
+    updateSubtotal()
+}
+
+function updateSubtotal(){
+    subtotalAll = 0
+    for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].currency === "UYU") {subtotalAll += Math.round(cartItems[i].subtotal / 41.20)}
+        //Al día de hoy, 26 de octubre, el dólar está a 41.20 pesos.
+        else {subtotalAll += cartItems[i].subtotal}
+    }
+    document.getElementById("subtotalAllCont").innerHTML = subtotalAll + " USD"
 }
 
 function showCart(){
